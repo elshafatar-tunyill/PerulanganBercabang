0bef935bcfdbf609a452a70e1d8d4cf5e8028c1e
elshafatar-tunyill <elshafatar@gmail.com>
2025-10-29 12:12:45 +0700

    percobaan 2.2

---
+++ Star08.java
@@ -0,0 +1,15 @@
+package jobsheet5;
+import java.util.Scanner;
+public class Star08 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nilai n = ");
+        int n = sc.nextInt();
+
+        for (int i= 1; i <= n; i++) {
+            System.out.print("*");
+        }
+        sc.close();
+     }
+}
+
